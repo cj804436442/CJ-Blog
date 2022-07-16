3fe3b13e6680d40ae14e5a31e9d923806ec543dc
cuijie <804436442@qq.com>
2022-07-16 23:49:29 +0800

    fix

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <Loading :visible="isLoading" />
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Loading from "../common/components/loading.vue"
+export default {
+  name: 'App',
+  components: {
+    Loading
+  },
+  data() {
+    return {
+      isLoading: false,
+    }
+  },
+  mounted() {
+    this.$root.$on("loading", (is) => {
+      this.isLoading = is
+    })
+  },
+}
+</script>
+
+<style lang="less" scoped>
+#app {
+  width: 100%;
+  min-height: 100%;
+  background: #f7f9fd;
+}
+</style>
